ec17129545ceb6fd9711444777017a5613d355e8
stajer <stajer@APS-05>
2022-03-24 14:59:17 +0700

    Add IUnitOfWork.

---
+++ AutoPartsStore.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using AutoPartsStore.DAL.Entities;
+namespace AutoPartsStore.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Brand> Brands { get; }
+        IRepository<Detail> Details { get; }
+        IRepository<Feature> Features { get; }
+        IRepository<Manufacturer> Manufacturers { get; }
+        IRepository<Model> Models { get; }
+        IRepository<Modification> Modifications { get; }
+        IRepository<Order> Orders { get; }
+        IRepository<PriceList> PriceLists { get; }
+        IRepository<Section> Sections { get; }
+        IRepository<Status> Statuses { get; }
+        IRepository<TypeDetail> TypeDetails { get; }
+        IRepository<TypeTransport> TypeTransports { get; }
+        IRepository<User> Users { get; }
+        void Save();
+    }
+}
