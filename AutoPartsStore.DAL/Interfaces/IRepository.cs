4f71946f6dcfce6e589b4d50b282a8d812c8ff63
stajer <stajer@APS-05>
2022-03-24 14:52:29 +0700

    Add IRepository.

---
+++ AutoPartsStore.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoPartsStore.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        void Create(T item);
+        void Update(T item);
+        void Delete(Guid id);
+    }
+}
