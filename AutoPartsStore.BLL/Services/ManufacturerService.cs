015eed60e0e204adb75dec57360ea4e6118f7ceb
stajer <stajer@APS-05>
2022-03-25 17:00:28 +0700

    Add ManufacturerService.

---
+++ AutoPartsStore.BLL/Services/ManufacturerService.cs
@@ -0,0 +1,13 @@
+﻿using AutoPartsStore.AN.DTO;
+using AutoPartsStore.AN.Entities;
+using AutoPartsStore.DAL.Interfaces;
+
+namespace AutoPartsStore.BLL.Services
+{
+    public class ManufacturerService : BaseService<ManufacturerDTO, Manufacturer>
+    {
+        public ManufacturerService(IUnitOfWork uow) : base(uow)
+        {
+        }
+    }
+}
