079c496443fb523ca9373afdacf2535de50de36f
stajer <stajer@APS-05>
2022-03-25 17:02:39 +0700

    Add ModelService.

---
+++ AutoPartsStore.BLL/Services/ModelService.cs
@@ -0,0 +1,13 @@
+﻿using AutoPartsStore.AN.DTO;
+using AutoPartsStore.AN.Entities;
+using AutoPartsStore.DAL.Interfaces;
+
+namespace AutoPartsStore.BLL.Services
+{
+    public class ModelService : BaseService<ModelDTO, Model>
+    {
+        public ModelService(IUnitOfWork uow) : base(uow)
+        {
+        }
+    }
+}
