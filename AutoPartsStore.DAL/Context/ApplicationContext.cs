32325438b800b333cc624140027d4c5327303927
stajer <stajer@APS-05>
2022-03-25 14:58:55 +0700

    Supress.

--- AutoPartsStore.DAL/Context/ApplicationContext.cs
+++ AutoPartsStore.DAL/Context/ApplicationContext.cs
@@ -21,7 +21,9 @@ namespace AutoPartsStore.DAL.EF
         public DbSet<TypeTransport>? TypeTransports { get; set; }
         public DbSet<User>? Users { get; set; }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
 
         }
