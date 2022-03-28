3da7579d8a1c7fca7815f9dec7378b4806707d50
stajer <stajer@APS-05>
2022-03-28 15:12:09 +0700

    Add TypeTransportService.

---
+++ AutoPartsStore.BLL/Services/TypeTransportService.cs
@@ -0,0 +1,13 @@
+﻿using AutoPartsStore.AN.DTO;
+using AutoPartsStore.AN.Entities;
+using AutoPartsStore.DAL.Interfaces;
+
+namespace AutoPartsStore.BLL.Services
+{
+    public class TypeTransportService : BaseService<TypeTransportDTO, TypeTransport>
+    {
+        public TypeTransportService(IUnitOfWork uow) : base(uow)
+        {
+        }
+    }
+}
