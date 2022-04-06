a48797bccf7754f11ea8645c14dd303a6eb17d07
stajer <stajer@APS-05>
2022-04-06 14:06:53 +0700

    Change Remove method.

--- AutoPartsStore.BLL/Interfaces/IService.cs
+++ AutoPartsStore.BLL/Interfaces/IService.cs
@@ -3,14 +3,13 @@ using AutoPartsStore.AN.Entities.Base;
 using AutoPartsStore.BLL.Services;
 
 namespace AutoPartsStore.BLL.Interfaces {
-    public interface IService<TEntity, TEntityDTO, TKey, TFilter>
+    public interface IService<TEntity, TEntityDTO, TKey, TFilter> : IDisposable
         where TEntityDTO : BaseEntityDTO<TKey>
         where TEntity : BaseEntity<TKey> {
-        ServiceResult Create(TEntityDTO entityDTO);
+        ServiceResult<TEntityDTO> Create(TEntityDTO entityDTO);
         ServiceResult<TEntityDTO> Get(TKey id);
         ServiceResult<IEnumerable<TEntityDTO>> GetAll(TFilter filter);
-        ServiceResult Remove(TEntityDTO entityDTO);
-        ServiceResult Update(TEntityDTO entityDTO);
-        void Dispose();
+        ServiceResult Remove(TKey id);
+        ServiceResult<TEntityDTO> Update(TEntityDTO entityDTO);
     }
 }
